bbf355492d452e0f05969d2cbed989d0d253dad9
Selen Kaptanoglu <154344954+selenkaptanoglu@users.noreply.github.com>
2024-11-04 13:38:13 +0300

    Add files via upload

---
+++ Quiz/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblQuestions = new System.Windows.Forms.Label();
+            this.LblFalse = new System.Windows.Forms.Label();
+            this.LblTrue = new System.Windows.Forms.Label();
+            this.BtnContinue = new System.Windows.Forms.Button();
+            this.BtnA = new System.Windows.Forms.Button();
+            this.BtnC = new System.Windows.Forms.Button();
+            this.BtnB = new System.Windows.Forms.Button();
+            this.BtnD = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(400, 172);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(434, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Questions:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(486, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "True:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(481, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "False:";
+            // 
+            // LblQuestions
+            // 
+            this.LblQuestions.AutoSize = true;
+            this.LblQuestions.Location = new System.Drawing.Point(553, 24);
+            this.LblQuestions.Name = "LblQuestions";
+            this.LblQuestions.Size = new System.Drawing.Size(24, 28);
+            this.LblQuestions.TabIndex = 10;
+            this.LblQuestions.Text = "0";
+            // 
+            // LblFalse
+            // 
+            this.LblFalse.AutoSize = true;
+            this.LblFalse.Location = new System.Drawing.Point(553, 102);
+            this.LblFalse.Name = "LblFalse";
+            this.LblFalse.Size = new System.Drawing.Size(24, 28);
+            this.LblFalse.TabIndex = 11;
+            this.LblFalse.Text = "0";
+            // 
+            // LblTrue
+            // 
+            this.LblTrue.AutoSize = true;
+            this.LblTrue.Location = new System.Drawing.Point(553, 61);
+            this.LblTrue.Name = "LblTrue";
+            this.LblTrue.Size = new System.Drawing.Size(24, 28);
+            this.LblTrue.TabIndex = 12;
+            this.LblTrue.Text = "0";
+            // 
+            // BtnContinue
+            // 
+            this.BtnContinue.BackColor = System.Drawing.Color.MistyRose;
+            this.BtnContinue.Location = new System.Drawing.Point(439, 159);
+            this.BtnContinue.Name = "BtnContinue";
+            this.BtnContinue.Size = new System.Drawing.Size(210, 37);
+            this.BtnContinue.TabIndex = 13;
+            this.BtnContinue.Text = "Continue >>";
+            this.BtnContinue.UseVisualStyleBackColor = false;
+            this.BtnContinue.Click += new System.EventHandler(this.BtnContinue_Click);
+            // 
+            // BtnA
+            // 
+            this.BtnA.Location = new System.Drawing.Point(16, 216);
+            this.BtnA.Name = "BtnA";
+            this.BtnA.Size = new System.Drawing.Size(191, 37);
+            this.BtnA.TabIndex = 14;
+            this.BtnA.Text = "A";
+            this.BtnA.UseVisualStyleBackColor = true;
+            this.BtnA.Click += new System.EventHandler(this.BtnA_Click);
+            // 
+            // BtnC
+            // 
+            this.BtnC.Location = new System.Drawing.Point(16, 268);
+            this.BtnC.Name = "BtnC";
+            this.BtnC.Size = new System.Drawing.Size(191, 37);
+            this.BtnC.TabIndex = 15;
+            this.BtnC.Text = "C";
+            this.BtnC.UseVisualStyleBackColor = true;
+            this.BtnC.Click += new System.EventHandler(this.BtnC_Click);
+            // 
+            // BtnB
+            // 
+            this.BtnB.Location = new System.Drawing.Point(225, 216);
+            this.BtnB.Name = "BtnB";
+            this.BtnB.Size = new System.Drawing.Size(191, 37);
+            this.BtnB.TabIndex = 16;
+            this.BtnB.Text = "B";
+            this.BtnB.UseVisualStyleBackColor = true;
+            this.BtnB.Click += new System.EventHandler(this.BtnB_Click);
+            // 
+            // BtnD
+            // 
+            this.BtnD.Location = new System.Drawing.Point(225, 268);
+            this.BtnD.Name = "BtnD";
+            this.BtnD.Size = new System.Drawing.Size(191, 37);
+            this.BtnD.TabIndex = 17;
+            this.BtnD.Text = "D";
+            this.BtnD.UseVisualStyleBackColor = true;
+            this.BtnD.Click += new System.EventHandler(this.BtnD_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(439, 216);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(553, 216);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(92, 89);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 28);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 28);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(666, 321);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnD);
+            this.Controls.Add(this.BtnB);
+            this.Controls.Add(this.BtnC);
+            this.Controls.Add(this.BtnA);
+            this.Controls.Add(this.BtnContinue);
+            this.Controls.Add(this.LblTrue);
+            this.Controls.Add(this.LblFalse);
+            this.Controls.Add(this.LblQuestions);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblQuestions;
+        private System.Windows.Forms.Label LblFalse;
+        private System.Windows.Forms.Label LblTrue;
+        private System.Windows.Forms.Button BtnContinue;
+        private System.Windows.Forms.Button BtnA;
+        private System.Windows.Forms.Button BtnC;
+        private System.Windows.Forms.Button BtnB;
+        private System.Windows.Forms.Button BtnD;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Quiz/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int questions = 0, trueQuestion = 0, falseQuestion = 0;
+        private void BtnContinue_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = true;
+            BtnB.Enabled = true;
+            BtnC.Enabled = true;
+            BtnD.Enabled = true;
+            BtnContinue.Enabled = false;
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+
+            questions++;
+            LblQuestions.Text = questions.ToString();
+            if (questions == 1)
+            {
+                richTextBox1.Text = "Which of the following is the SQL function that rounds the decimal part of a specified field?";
+                BtnA.Text = "A) Update";
+                BtnB.Text = "B) Alter";
+                BtnC.Text = "C) Round";
+                BtnD.Text = "D) Revoke";
+                label4.Text = "C) Round";
+
+            }
+            if (questions == 2)
+            {
+                richTextBox1.Text = "Which of the following is the command used to search for multiple values in a specified single field?";
+                BtnA.Text = "A) Search";
+                BtnB.Text = "B) In";
+                BtnC.Text = "C) As";
+                BtnD.Text = "D) Round";
+                label4.Text = "B) In";
+            }
+            if (questions == 3)
+            {
+                richTextBox1.Text = "Which of the following command lines prints the first 4 letters of the word 'America'?";
+                BtnA.Text = "A) Select Left(4,'America')";
+                BtnB.Text = "B) Select Substring('America',4)";
+                BtnC.Text = "C) Select Right('America',4)";
+                BtnD.Text = "D) Select Left('America',4)";
+                label4.Text = "D) Select Left('America',4)";
+            }
+            if (questions == 4)
+            {
+                richTextBox1.Text = "What is the purpose of the command SELECT * FROM CUSTOMERS WHERE ID IN (SELECT CUSTOMERID FROM ADDRESS GROUP BY CUSTOMERID HAVING COUNT(CUSTOMERID) > 2)";
+                BtnA.Text = "A) To retrieve customers with more than 2 address";
+                BtnB.Text = "B) To retrieve customers with only 2 addresses";
+                BtnC.Text = "C) To retrieve all customers without an address";
+                BtnD.Text = "D) To retrieve all customers with an address";
+                label4.Text = "A) To retrieve customers with more than 2 address";
+            }
+            if (questions == 5)
+            {
+                richTextBox1.Text = "What does SQL Server Instance mean?";
+                BtnA.Text = "A) Each SQL Server service running on a server is an instance";
+                BtnB.Text = "B) It is the administrator account information of SQL Server";
+                BtnC.Text = "C) It is the IP information of SQL Server";
+                BtnD.Text = "D) It is the installation language information of SQL Server";
+                label4.Text = "A) Each SQL Server service running on a server is an instance";
+            }
+            if (questions == 6)
+            {
+                BtnA.Enabled = false;
+                BtnB.Enabled = false;
+                BtnC.Enabled = false;
+                BtnD.Enabled = false;
+                BtnContinue.Enabled = false;
+                MessageBox.Show("True: " + trueQuestion + "\n" + "False: " + falseQuestion); ;
+            }
+        }
+        private void BtnA_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnContinue.Enabled = true;
+
+            label5.Text = BtnA.Text;
+            if (label4.Text == label5.Text)
+            {
+                trueQuestion++;
+                LblTrue.Text = trueQuestion.ToString();
+                BtnA.BackColor = Color.Green;
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                falseQuestion++;
+                LblFalse.Text = falseQuestion.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+        private void BtnB_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnContinue.Enabled = true;
+
+            label5.Text = BtnB.Text;
+            if (label4.Text == label5.Text)
+            {
+                trueQuestion++;
+                LblTrue.Text = trueQuestion.ToString();
+                BtnB.BackColor = Color.Green;
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                falseQuestion++;
+                LblFalse.Text = falseQuestion.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+        private void BtnC_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnContinue.Enabled = true;
+
+            label5.Text = BtnC.Text;
+            if (label4.Text == label5.Text)
+            {
+                trueQuestion++;
+                LblTrue.Text = trueQuestion.ToString();
+                BtnC.BackColor = Color.Green;
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                falseQuestion++;
+                LblFalse.Text = falseQuestion.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+        private void BtnD_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnContinue.Enabled = true;
+
+            label5.Text = BtnD.Text;
+            if (label4.Text == label5.Text)
+            {
+                trueQuestion++;
+                LblTrue.Text = trueQuestion.ToString();
+                BtnD.BackColor = Color.Green;
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                falseQuestion++;
+                LblFalse.Text = falseQuestion.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+    }
+}
